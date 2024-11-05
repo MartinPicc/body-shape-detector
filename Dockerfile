4ea5a887648981b8021a81b6a5cc7d235d450fe6
MartinPicc <martin.piccolin@hotmail.fr>
2024-11-05 18:35:50 +0100

    optimize dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM runpod/pytorch:2.1.0-py3.10-cuda11.8.0-devel-ubuntu22.04
 WORKDIR /app
 
 RUN apt update \
-  && apt install -y wget zip unzip libturbojpeg libglfw3-dev libgles2-mesa-dev ninja-build
+  && apt install -y --no-install-recommends wget zip unzip libturbojpeg libglfw3-dev libgles2-mesa-dev ninja-build \
+  && rm -rf /var/lib/apt/lists/*
 
 COPY ./requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
@@ -14,11 +15,13 @@ ENV EGL_DEVICE_ID=1
 # ENV
 
 COPY . .
-RUN cd ./attributes \
-  && python setup.py install
 ARG TORCH_CUDA_ARCH_LIST="3.7;5.0;6.0;7.0;7.5;8.0;8.6;9.0+PTX"
+RUN cd ./attributes \
+  && python setup.py install \
+  && rm -rf ./build ./dist ./*.egg-info
 RUN cd ./mesh-mesh-intersection \
-  && python setup.py install
+  && python setup.py install \
+  && rm -rf ./build ./dist ./*.egg-info
 
 ARG shapy_usr
 ARG shapy_pwd
--- data/download_models.sh
+++ data/download_models.sh
@@ -16,12 +16,12 @@ username=$(urle "$username")
 password=$(urle "$password")
 
 # Download SHAPY data
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=shapy&resume=1&sfile=shapy_data.zip' -O 'shapy_data.zip' --no-check-certificate --continue
+wget --progress=dot:giga --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=shapy&resume=1&sfile=shapy_data.zip' -O 'shapy_data.zip' --no-check-certificate --continue
 unzip shapy_data.zip
 rm shapy_data.zip
 
 # Download SMPL model
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=models_smplx_v1_1.zip&resume=1' -O 'models_smplx_v1_1.zip' --no-check-certificate --continue
+wget --progress=dot:giga --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=models_smplx_v1_1.zip&resume=1' -O 'models_smplx_v1_1.zip' --no-check-certificate --continue
 unzip models_smplx_v1_1.zip
 mv models body_models
 rm models_smplx_v1_1.zip
